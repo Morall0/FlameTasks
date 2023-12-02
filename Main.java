5f75faf6228497b66d25b103063d474d30b10534
Maurici0ML <alanmauriciomoraleslopez@gmail.com>
2023-12-02 16:38:59 -0600

    feat: esqueleto Main

--- Main.java
+++ Main.java
@@ -1,8 +1,54 @@
 import usuarios.Usuario;
+
+import java.util.Scanner;
+
 import autent.Autenticacion;
 
 public class Main {
+    private static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
+        int opt;
         Usuario usuario = Autenticacion.autenticacion();
+
+        do {
+            
+            System.out.println("--- FlameTask ---");
+            System.out.println("1. Crear Tarea");
+            System.out.println("2. Ver tareas ");
+            System.out.println("3. Crear proyecto");
+            System.out.println("4. Ver proyecto");
+            System.out.println("5. Salir");
+            System.out.print("> ");
+            opt = scan.nextInt();
+            scan.nextLine();
+    
+            switch (opt) {
+                case 1:
+                    
+                    break;
+
+                case 2:
+                    
+                    break;
+
+                case 3:
+                    
+                    break;
+
+                case 4:
+                    
+                    break;
+
+                case 5:
+                    
+                    break;
+
+                default:
+                    System.out.println("\nSelecciona una opción válida!\n");
+                    break;
+            }
+        } while (opt != 5);
+
+        scan.close();
     }
 }
